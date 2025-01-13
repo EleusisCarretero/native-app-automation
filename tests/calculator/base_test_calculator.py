5f184586348954392c3ea0475ccce88eb34ed228
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-13 17:55:12 -0600

    feat: create base calculator class

---
+++ tests/calculator/base_test_calculator.py
@@ -0,0 +1,9 @@
+
+from apps.calculator_app import CalculatorApp
+
+
+class BaseTestCalculator:
+
+    def setup(self, driver):
+        self.driver_manager = driver
+        self.cal_iface = CalculatorApp(self.driver_manager.driver)
