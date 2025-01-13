842900038718fd0f874bb75c26c175e605aff401
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-12 23:36:35 -0600

    feat locators for result and equals

--- utils/locators.py
+++ utils/locators.py
@@ -1,6 +1,15 @@
 from appium.webdriver.common.appiumby import AppiumBy
 class CalculatorLocators:
-    BASED_XPATH = "//android.widget.Button[@content-desc='{button}']"
+    BASED_BTTN_XPATH = "//android.widget.Button[@content-desc='{button}']"
+    RESULT_ID = "com.sec.android.app.popupcalculator:id/calc_edt_formula"
     @classmethod
     def get_num_locator(cls, num):
-        return (AppiumBy.XPATH, cls.BASED_XPATH.format(button=num))
+        return (AppiumBy.XPATH, cls.BASED_BTTN_XPATH.format(button=num))
+
+    @classmethod
+    def get_op_locator(cls, op):
+        return (AppiumBy.XPATH, cls.BASED_BTTN_XPATH.format(button=op))
+    
+    @classmethod
+    def get_result(cls):
+        return (AppiumBy.ID, cls.RESULT_ID)
