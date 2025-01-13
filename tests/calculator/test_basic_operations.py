e352ced90c0afa51c88d89944ccfb12130ed4cbb
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-13 17:12:01 -0600

    feat:include test for mul and div

--- tests/calculator/test_basic_operations.py
+++ tests/calculator/test_basic_operations.py
@@ -4,7 +4,7 @@ import pytest
 from appium.webdriver.common.appiumby import AppiumBy
 
 from apps.calculator_app import CalculatorApp
-from utils.tools import YamlManager
+from utils.tools import YamlManager, div_seq_number, mul_sep_number, subs_seq_number, sum_seq_number
 
 
 @pytest.fixture(scope="class")
@@ -22,12 +22,68 @@ class TestBasicOperations:
         self.driver_manager = driver
         self.cal_iface = CalculatorApp(self.driver_manager.driver)
     
-    def test_simple_addition(self):
+    
+    
+    @pytest.mark.parametrize(
+            ("number"),
+            [
+                (897),
+                (87531),
+                (753149)
+            ]
+    )
+    def test_simple_addition(self, number):
+        """
+        Test case to evaluate a simple sum of unit numbers.
 
-        result = self.cal_iface.single_seq_operation(num=87596, operations=["+" for _ in range(5)])
-        assert int(result) == (8+7+5+9+6)
+        Args:
+            number(int): Sequence of unit numbers
+        """
+        len_operation = len(str(number)) - 1
+        result = self.cal_iface.single_seq_operation(num=number, operations=["+" for _ in range(len_operation)])
+        assert int(result) == sum_seq_number(number)
 
-    def test_simple_substraction(self):
-        result = self.cal_iface.single_seq_operation(num=87596, operations=["-" for _ in range(5)])
-        assert int(result) == (8-7-5-9-6)
+    @pytest.mark.parametrize(
+            ("number"),
+            [
+                (897),
+                (87531),
+                (753149)
+            ]
+    )
+    def test_simple_subtraction(self, number):
+        """
+        Test case to evaluate a simple subtraction of unit numbers.
 
+        Args:
+            number(int): Sequence of unit numbers
+        """
+        len_operation = len(str(number)) - 1
+        result = self.cal_iface.single_seq_operation(num=number, operations=["-" for _ in range(len_operation)])
+        assert int(result) == subs_seq_number(number)
+
+    @pytest.mark.parametrize(
+            ("number"),
+            [
+                (897),
+                (87531),
+                (753149)
+            ]
+    )
+    def test_simple_mul(self, number):
+        len_operation = len(str(number)) - 1
+        result = self.cal_iface.single_seq_operation(num=number, operations=["*" for _ in range(len_operation)])
+        assert int(result) == mul_sep_number(number)
+    
+    @pytest.mark.parametrize(
+            ("number"),
+            [
+                (897),
+                (87531),
+                (753149)
+            ]
+    )
+    def test_simple_div(self, number):
+        len_operation = len(str(number)) - 1
+        result = self.cal_iface.single_seq_operation(num=number, operations=["/" for _ in range(len_operation)])
+        assert int(result) == div_seq_number(number)
