08304189ba7e2dbc9fd2e2f3446485f230d7f25d
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-12 23:36:07 -0600

    feat: return text value

--- apps/base_app.py
+++ apps/base_app.py
@@ -5,3 +5,8 @@ class BaseApp:
     def click_app_button(self, locator):
         button_obj = self.base_driver.find_element(*locator)
         button_obj.click()
+    
+    def get_text(self, locator):
+        obj = self.base_driver.find_element(*locator)
+        return obj.text
+
