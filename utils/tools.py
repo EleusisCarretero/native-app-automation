ecfa60e20080fc6d48cbd0a4172140025aefe2a4
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-14 13:00:03 -0600

    style: creation of math utils class to contain arithmetic_operation_seq_number

--- utils/tools.py
+++ utils/tools.py
@@ -2,6 +2,7 @@
 Contains common functions, constants, classes that can be util but not
 necessary is part of a feature.
 """
+import re
 import os
 import yaml
 
@@ -21,14 +22,26 @@ class YamlManager:
             content = yaml.safe_load(file)
         return content
 
+class MathUtilsError(Exception):
+    pass
 
-def arithmetic_operation_seq_number(sequence_num, arithmetic_op):
-    res = 0
-    for i, num in enumerate(str(sequence_num)):
-        if i == 0:
-            res = float(num)
-        else:
-            res = eval(f"{res}{arithmetic_op[i-1]}{num}")
-    return res
+class MathUtils:
+
+    @staticmethod
+    def arithmetic_operation_seq_number(sequence_num, arithmetic_op):
+        pattern_num = r"\d*"
+        try:
+            str_sequence_num = str(sequence_num)
+        except TypeError:
+            raise MathUtilsError("Unable to convert {sequence_num} to str")
+        if not re.search(pattern_num, str_sequence_num):
+            raise MathUtilsError("'arithmetic_op' should be a string of number between 0-9")
+        res = 0
+        for i, num in enumerate(str_sequence_num):
+            if i == 0:
+                res = float(num)
+            else:
+                res = eval(f"{res}{arithmetic_op[i-1]}{num}")
+        return res
 
 
