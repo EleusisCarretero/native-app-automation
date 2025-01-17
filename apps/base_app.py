3f69c5b5f43b4fb9885b010d0c23125f00aa8a14
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-17 00:18:04 -0600

    docs: docstrings

--- apps/base_app.py
+++ apps/base_app.py
@@ -1,31 +1,74 @@
+"""
+Base class file
+"""
 from utils.zoom import Zoom
 
 
 class BaseAppError(Exception):
+    """
+    BaseApp Exception
+    """
     pass
 
 
 class BaseApp:
+    """
+    Base class to control an app
+
+    Attributes:
+        base_driver (WebDriver): Webdriver instance of the current session.
+    """
     def __init__(self, driver):
         self.base_driver = driver
     
     def click_app_button(self, locator):
+        """
+        Click on the object that matches the 'locator'
+
+        Args:
+            locator (tuple): type of identifier (AppiumID), value to look for (str)
+        """
         button_obj = self.base_driver.find_element(*locator)
         button_obj.click()
     
     def get_text(self, locator):
+        """
+        Gets the text from the element which matches with the locator.
+
+         Args:
+            locator (tuple): type of identifier (AppiumID), value to look for (str)
+
+        Returns:
+            str: text from the object found
+        """
         obj = self.base_driver.find_element(*locator)
         return obj.text
 
-    def get_list_of_elements(self,  locator):
-        list_obj = self.base_driver.find_elements(*locator)
+    def get_list_of_elements(self, locator):
+        """
+        Gets a list of all the elements which matches with the 'locator'
+
+        Args:
+            locator (tuple): type of identifier (AppiumID), value to look for (str)
+
+        Returns:
+            list: list of object which matched with the locator
+        """
+        try:
+            list_obj = self.base_driver.find_elements(*locator)
+        except Exception as e:
+            raise BaseAppError(f"Unable to found a list of objects by using the locator {locator}") from e
         return list_obj
 
     def make_zoom(self, locator, percentage=0.5, velocity=1.0, expand=True):
         """
+        Makes zoom in an object, it can ben positive or negative.
 
-        Run appium --use-plugins=gestures
-
+         Args:
+            locator (Tuple): locator of the element to zooming
+            percentage (float): Percentage of zoom
+            velocity (float): velocity to perform the zoom
+            expand (boolean): flag to indicate the sense of the zooming
         """
         Zoom.make_zoom(
             driver=self.base_driver,
@@ -34,12 +77,22 @@ class BaseApp:
             velocity=velocity,
             expand=expand)
 
-    def stepping_zoom(self, locator, stepping ,percentage, velocity, expand=True):
+    def stepping_zoom(self, locator, stepping ,percentage, velocity, expand=True, wait_time=0.1):
+        """
+         Args:
+            locator (Tuple): locator of the element to zooming
+            stepping (float): value to stepping
+            percentage (float): Percentage of zoom
+            velocity (float): velocity to perform the zoom
+            expand (boolean): flag to indicate the sense of the zooming
+            wait_time (float): time in seconds to wait before to the next zoom.
+
+        """
         Zoom.stepping_zoom(
             driver=self.base_driver,
             locator=locator,
             stepping=stepping,
             percentage=percentage,
             velocity=velocity,
-            expand=expand)
-
+            expand=expand,
+            wait_time=wait_time)
