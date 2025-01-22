04192cae56e53159e91b93b49f5deb6d0de1bde2
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-21 19:16:56 -0600

    docs including docsring

--- utils/scroll.py
+++ utils/scroll.py
@@ -1,13 +1,21 @@
-import time
-
-from appium.webdriver.common.appiumby import AppiumBy
-
+from enum import Enum
 from utils.zoom import MobileCommands
 
+
+class ScrollDirection(str, Enum):
+    """Enum to control scroll direction"""
+    UP = "up"
+    DOWN = "down"
+
+
 class ScrollError(Exception):
-    pass
+    """Scroll Error"""
+
 
 class Scroll:
+    """
+    Gesture class to scroll in the app screen
+    """
     SCROLL_STEP = MobileCommands(f"scrollGesture", elementId='{id}', direction='{direction}', percent='{percent}')
 
     @classmethod
@@ -17,7 +25,25 @@ class Scroll:
         )
 
     @classmethod
-    def scroll_until(cls, looking_element, driver, locator, max_scrolls=60, direction="down", percent=0.3, specific_text=""):
+    def scroll_until(cls, looking_element, driver, locator, max_scrolls=60, direction=ScrollDirection.DOWN.value, percent=0.3, specific_text=""):
+        """
+        Method to scroll a screen until some element has been found, or until the element has the corresponding text.
+
+        Args:
+            looking_element (): App element to look for the desired value and scrolling
+            driver(WebDriver): Web driver element
+            locator(tuple): Couple of values to look for the desired element
+            max_scrolls(int): number of max scrolls until stop if the element hasn't been found
+            direction(ScrollDirection): direction of scroll, up or down
+            percent(float): Percentage to do the scroll in the, previously defined direction
+            specific_text(str/Optional): expected text value to found the element, it is optional.
+
+        Returns:
+            Webdriver: Element found which covers the conditions
+        
+        Exception:
+            ScrollError: The element haven't been found.
+        """
         found_element = None
         count_scroll = 1
         while count_scroll <= max_scrolls:
@@ -34,4 +60,3 @@ class Scroll:
         else:
             print("The element hasn't been found")
         raise ScrollError(f"Not found element")
-        
