4cd0ac732212bb3ec6a52d1fccc1ce586cdbabe1
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-23 23:44:32 -0600

    style&fix: change inheritace line:BaseTestCalculator

--- tests/calculator/base_test_calculator.py
+++ tests/calculator/base_test_calculator.py
@@ -1,10 +1,10 @@
-import pytest
-from enum import Enum
+
 from apps.calculator_app import CalculatorApp
+from tests.base_test import BaseTest
 
 
-class BaseTestCalculator:
+class BaseTestCalculator(BaseTest):
 
     def setup(self, driver):
-        self.driver_manager = driver
+        # self.driver_manager = driver
         self.cal_iface = CalculatorApp(self.driver_manager.driver)
