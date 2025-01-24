52e328333f07b514f962c6dddb88606109ef8105
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-24 00:08:37 -0600

    docs: missing docstring

--- tests/camara/base_test_camara.py
+++ tests/camara/base_test_camara.py
@@ -1,18 +1,23 @@
-import pytest
+"""
+BaseTestCamara file and relates
+"""
 from enum import Enum
 from apps.camara_app import CamaraApp
 from tests.base_test import BaseTest
-from utils.tools import YamlManager
-
-
-
 
 
 class CameraType(str, Enum):
+    """Type camera"""
     FRONTAL = "Frontal"
     NORMAL = "Normal"
 
 class BaseTestCamara(BaseTest):
+    """
+    Paren of all test camera classes
+
+    Attributes:
+        camara_iface(CamaraApp): instance interface for app
+    """
     def setup(self, driver):
         super().setup(driver)
         self.camara_iface = CamaraApp(self.driver_manager.driver)
