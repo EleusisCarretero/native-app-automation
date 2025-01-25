7a9441141d3d6a10864be78b740435e76a923ce8
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-24 18:59:05 -0600

    docs: missing docstrings

--- tests/unittest/test_utils/test_tools.py
+++ tests/unittest/test_utils/test_tools.py
@@ -1,14 +1,22 @@
+"""
+Test file to validate tools.py file
+"""
 import unittest
 from utils.tools import MathUtils, MathUtilsError
 from parameterized import parameterized
 
 class InvalidStrObject:
+    """Invalid object to make an invalid str conversion"""
     def __init__(self, value):
         self._value = value
+
     def __str__(self):
         return int(self._value)
 
 class TestMathUtils(unittest.TestCase):
+    """
+    Unit test class to validate MathUtils class.
+    """
 
     @parameterized.expand(
         [
@@ -19,6 +27,15 @@ class TestMathUtils(unittest.TestCase):
         ]
     )
     def test_valid_value(self, name, numbers, operations, expected_result):
+        """
+        Validate valid values for arithmetic_operation_seq_number method
+    
+        Args:
+            name(str): Type of operation, just informative
+            numbers(int): Sequence of numbers to perform operation
+            operations(list): list of operations to perform over numbers
+            expected_result(float/int): Expected result
+        """
         print(f"Testing: {name}")
         result = round(MathUtils.arithmetic_operation_seq_number(numbers, operations), 3)
         self.assertEqual(result, expected_result)
@@ -33,6 +50,12 @@ class TestMathUtils(unittest.TestCase):
         ]
     )
     def test_invalid_pattern(self, invalid_object):
+        """
+        Test to validate invalid parameters to enter to arithmetic_operation_seq_number
+
+        Args:
+            invalid_object(Any): Invalid type of input
+        """
         operations = ["+" for _ in range(3)]
         with self.assertRaises(expected_exception=MathUtilsError) as e:
             MathUtils.arithmetic_operation_seq_number(invalid_object,operations)
@@ -40,14 +63,16 @@ class TestMathUtils(unittest.TestCase):
         self.assertEqual(actual_exception, MathUtilsError)
 
     def test_type_error(self):
+        """
+        Validate arithmetic_operation_seq_number raises the expected exception
+        when an invalid object, which cannot be converted to str is introduce
+        """
         invalid_obj = InvalidStrObject("3")
         operations = ["+" for _ in range(3)]
         with self.assertRaises(expected_exception=TypeError) as e:
             MathUtils.arithmetic_operation_seq_number(invalid_obj, operations)
         actual_exception = e.expected
         self.assertEqual(actual_exception, TypeError)
-
-
 
 
 if __name__ == "__main__":
