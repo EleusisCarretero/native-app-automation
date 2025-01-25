ae8c5ff4dc88797ed81edd710f4cf71ca40987db
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-24 19:42:25 -0600

    docs: misisng docstrings

--- apps/gallery_app.py
+++ apps/gallery_app.py
@@ -1,15 +1,30 @@
+"""
+Gallery app file
+"""
 from apps.base_app import BaseApp
 from utils.locators import GalleryLocators
 
 
 class GalleryApp(BaseApp):
+    """
+    Gallery app class
+    """
     def __init__(self, driver):
         super().__init__(driver)
         self.sequence = None
 
     def click_info_button(self):
+        """
+        Click on info button
+        """
         self.click_app_button(GalleryLocators.get_info_locator())
 
     def get_picture_date(self):
+        """
+        Get data from the info of the picture focused
+
+        Returns:
+            str: text information
+        """
         obj = self.get_list_of_elements(GalleryLocators.get_basic_info_locator())[0]
         return obj.text
--- apps/tmp.py
+++
@@ -1,6 +0,0 @@
-# tmp_str ="{elementId: {id}, percent: {percentage}, velocity: {velocity}}"
-# new_str = tmp_str.format(id=40, percentage=0.3, velocity=1.0)
-# print(eval(new_str))
-example = "{{'{key}': {value}}}"
-new_str = example.format(key="10", value=10)
-print(eval(new_str))
