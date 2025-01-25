29323a45b6abf2c83d303aabfaf0c10b7735a7ea
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-24 19:07:12 -0600

    docs:missing docstrings

--- utils/driver.py
+++ utils/driver.py
@@ -1,18 +1,31 @@
-
+"""
+AppiumDriverManager class file and relates
+"""
 from appium.options.android import UiAutomator2Options
 from appium import webdriver
 
 
 class AppiumDriverManagerError(Exception):
-    pass
+    """
+    AppiumDriverManager Error
+    """
 
 
 class AppiumDriverManager:
+    """
+    Class to handle the needed driver settings
+
+    Attributes:
+        app_package(str): App package
+        app_activity(str): App Activity
+        device_name(str): Device name
+        kwargs(): Other app settings optionally included
+    """
     APPIUM_HOST = 'http://127.0.0.1:4723'
 
     def __init__(self, app_package, app_activity, device_name="192.168.1.64:5555", **kwargs):
         """
-        TODO
+        Initialize the driver with the given app settings
         """
         self.capabilities = {
             "platformName": "Android",
@@ -28,19 +41,25 @@ class AppiumDriverManager:
     
     @property
     def options(self):
+        """
+        Property 'options'
+        """
         return self._options
     
     def start_driver(self):
+        """
+        Start the set driver
+        """
         try:
             self.driver = webdriver.Remote(self.APPIUM_HOST, options=self.options)
         except Exception as e:
             raise AppiumDriverManagerError("Unable to start driver") from e
     
     def stop_driver(self):
-        # driver = driver or self.driver
+        """
+        Stop current driver
+        """
         try:
             self.driver.quit()
         except Exception as e:
             raise AppiumDriverManagerError ("Unable to stop driver") from e
-
-        
