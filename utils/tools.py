49d6354101c3dc880667a4d620e246f92a906d1a
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-24 19:26:19 -0600

    docs:missing docstrings

--- utils/tools.py
+++ utils/tools.py
@@ -18,22 +18,34 @@ class YamlManager:
         current_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         file_path = os.path.join(current_dir, relative_path)
         content = {}
-        with open(file_path, "r") as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             content = yaml.safe_load(file)
         return content
 
 class MathUtilsError(Exception):
-    pass
+    """MathUtils Error"""
 
 class MathUtils:
-
+    """
+    Math utils class
+    """
     @staticmethod
     def arithmetic_operation_seq_number(sequence_num, arithmetic_op):
+        """
+        Perform a sequence of simple arithmetic operations
+
+        Args:
+            sequence_num(int): Sequence of numbers
+            arithmetic_op(list): List of str with the arithmetic operation
+        
+        Returns:
+            int/float: Result of the operation
+        """
         pattern_num = r'^-?\d+(\.\d+)?$'
         try:
             str_sequence_num = str(sequence_num)
-        except TypeError:
-            raise MathUtilsError(f"Unable to convert {sequence_num} to str")
+        except TypeError as e:
+            raise MathUtilsError(f"Unable to convert {sequence_num} to str") from e
         if not re.match(pattern_num, str_sequence_num):
             raise MathUtilsError("'arithmetic_op' should be a string of number between 0-9")
         res = 0
@@ -41,11 +53,17 @@ class MathUtils:
             if i == 0:
                 res = float(num)
             else:
-                res = eval(f"{res}{arithmetic_op[i-1]}{num}")
+                res = eval(f"{res}{arithmetic_op[i-1]}{num}")  # noqa: W0123
         return res
     
     @staticmethod
     def increment_current_time(current_time, increment:float):
+        """
+        Increment the given time in hours, or/and minutes
+        Args:
+            current_time: Current time to increment
+            increment(float): Value in hr.min to make conversion, ex 1.3 -> 1h with 30 mins
+        """
         _hr, _min = str(increment).split(".")
         new_hr = current_time.hour + int(_hr)
         new_min = current_time.minute + int(_min)
